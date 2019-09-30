ce0b9ab60180d1dc36064eb878b97b603dacf842
yogaibnu <yogaibnudinataa@gmail.com>
2019-09-30 14:05:31 +0700

    bundle

--- app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
+++ app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
@@ -3,18 +3,25 @@ package id.ac.polinema.idealbodyweight;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import id.ac.polinema.idealbodyweight.fragments.AboutFragment;
+
 
 public class MainActivity extends AppCompatActivity {
 
 	// Deklarasikan atribut Fragment di sini
+	private AboutFragment aboutFragment;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		aboutFragment = AboutFragment.newInstance("Yoga Ibnudinata");
 	}
 
 	@Override
@@ -26,7 +33,13 @@ public class MainActivity extends AppCompatActivity {
 	@Override
 	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 		// TODO: Tambahkan penanganan menu di sini
-
+		if (item.getItemId() == R.id.menu_about) {
+			getSupportFragmentManager().beginTransaction()
+					.replace(R.id.fragment_container, aboutFragment)
+					.addToBackStack(null)
+					.commit();
+		}
 		return super.onOptionsItemSelected(item);
 	}
+
 }
